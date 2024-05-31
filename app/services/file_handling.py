e7b6170ece582e926cd9a345c1dcd3ccdb93799e
Rangga <ranggaaldosas@gmail.com>
2024-05-31 19:19:51 +0700

    feat: fixing /uploadfile endpoint

--- app/services/file_handling.py
+++ app/services/file_handling.py
@@ -4,9 +4,9 @@ from fastapi import UploadFile
 
 async def extract_text_from_pdf(upload_file: UploadFile) -> str:
     content = await upload_file.read()
-    pdf_reader = PyPDF2.PdfFileReader(upload_file.file)
+    pdf_reader = PyPDF2.PdfReader(upload_file.file)
     text = ""
     for page_num in range(len(pdf_reader.pages)):
-        page = pdf_reader.getPage(page_num)
-        text += page.extractText()
+        page = pdf_reader.pages[page_num]
+        text += page.extract_text()
     return text
